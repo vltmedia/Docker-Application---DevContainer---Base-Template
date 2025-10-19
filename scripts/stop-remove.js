1eb894d90522467ad2bc8da745712aef6eb987e5
vltmedia <info@vltmedia.com>
2025-10-19 01:05:08 -0400

    Bug: Stop Remove was exiting if the container didin't exist or errors.

--- scripts/stop-remove.js
+++ scripts/stop-remove.js
@@ -1,6 +1,12 @@
 // scripts/stop-remove.js
+console.log("Stopping and removing existing container if any...");
 const { loadConfig, sh } = require("./utils");
 const cfg = loadConfig();
+console.log(`Container name: ${cfg.appName}`);
 
-sh("docker", ["stop", cfg.appName], { stdio: "ignore" });
-sh("docker", ["rm", cfg.appName], { stdio: "ignore" });
+// Don't fail the whole script if the container isn't there.
+sh("docker", ["stop", "-t", "0", cfg.appName], { stdio: "ignore", allowFail: true });
+sh("docker", ["rm", "-f", cfg.appName],       { stdio: "ignore", allowFail: true });
+
+console.log(`Stopped and removed (if existed): ${cfg.appName}`);
+console.log("Done.");
